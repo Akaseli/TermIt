26ec6a6a2023b3a230aef0b8b7c337d5b63ef19c
FinFuz <60463103+Akaseli@users.noreply.github.com>
2023-10-21 10:25:53 +0300

    Added backend

---
+++ backend/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import http from "http"
+import cors from "cors"
+
+const app = express()
+const port = 3000
+const server = http.createServer(app)
+
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    credentials: true
+  })
+);
+
+app.get("/api/", (req, res) => {
+  res.json({online: true})
+})
+
+server.listen(port, () => {
+  console.log("Backend is up on port " + port)
+})
