9dc2fbf939156c9516852620341f05464797b27c
Akaseli <60463103+Akaseli@users.noreply.github.com>
2024-01-27 14:35:40 +0200

    Editing of terms

--- frontend/src/Pages/CreatePage.tsx
+++ frontend/src/Pages/CreatePage.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 
 import "./CreatePage.css"
 import { GradientButton } from '../components/GradientButton';
-import { TernInput } from '../components/TermInput';
+import { TermInput } from '../components/TermInput';
+import { Term } from '../app/types/term';
 
 interface Props {
 
@@ -12,6 +13,33 @@ interface Props {
 export const CreatePage: React.FC<Props> = () => {
   const { t, i18n } = useTranslation();
 
+  const [terms, setTerms] = useState<Term[]>([])
+
+  const addTerm = () => {
+    setTerms([...terms, {term: "", definition: ""}])
+  }
+
+  const removeTerm = (index: number) => {
+    const temp = [...terms]
+    temp.splice(index, 1)
+
+    setTerms(temp)
+  }
+
+  const modifyTerm = (index: number, value:Term) => {
+    const temp = [...terms]
+
+    temp[index] = value
+
+    setTerms(temp)
+  }
+
+  const cards = terms.map((term, index) => {
+    return (
+      <TermInput term={term} key={index} index={index} remove={removeTerm} modify={modifyTerm}/>
+    )
+  })
+
   return(
     <div className="column">
       <h1>{t('create')}</h1>
@@ -19,13 +47,13 @@ export const CreatePage: React.FC<Props> = () => {
       <div className='terms'>
         <div className="row header">
           <h2>{t("terms")}</h2>
-          <GradientButton>{t("add_term")}</GradientButton>
+          <GradientButton onClick={addTerm}>{t("add_term")}</GradientButton>
         </div>
 
         <div className='column'>
-          <TernInput index={1}/>
+          {cards}
         </div>
-        
+
       </div>
       
     </div>
--- frontend/src/components/TermInput.tsx
+++ frontend/src/components/TermInput.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./TermInput.css"
 import { useTranslation } from 'react-i18next';
+import { GradientButton } from './GradientButton';
+import { Term } from '../app/types/term';
 
 interface Props {
-  index: number
+  index: number,
+  term: Term,
+  remove: (index: number) => void,
+  modify: (index: number, value:Term) => void
 }
 
-export const TernInput: React.FC<Props> = ({index}) => {
+export const TermInput: React.FC<Props> = ({index,term,  remove, modify}) => {
   const { t, i18n } = useTranslation();
+
+  const termRef = useRef<HTMLInputElement>(null)
+  const defRef = useRef<HTMLInputElement>(null)
+
+  const handleChange = () => {
+    let term: Term = {term: termRef.current?.value ?? "", definition: defRef.current?.value ?? ""}
+    
+    modify(index, term)
+  }
 
   return(
     <div className="terminput">
-      <p>{index}</p>
-      <input placeholder={t("term")}></input>
-      <input placeholder={t("definition")}></input>
+      <p>{index+1}</p>
+      <input ref={termRef} onChange={handleChange} placeholder={t("term")} value={term.term}></input>
+      <input ref={defRef} onChange={handleChange} placeholder={t("definition")} value={term.definition}></input>
+      <GradientButton onClick={() => {remove(index)}}>Remove</GradientButton>
     </div>
   );
 }
